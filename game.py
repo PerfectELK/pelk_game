f4dba7fedfeee942ab20b5544be38ddf8030df38
Losev Andrey <Losikoban@Gmail.com>
2019-06-02 21:49:32 +0300

    walk and fire

--- game.py
+++ game.py
@@ -21,6 +21,7 @@ jumpCount = 10
 left = False
 right = False
 animCount = 0
+last_move = 'right'
 
 walkRight = [pygame.image.load('sprites/pygame_right_1.png'), pygame.image.load('sprites/pygame_right_2.png'), pygame.image.load('sprites/pygame_right_3.png'), pygame.image.load('sprites/pygame_right_4.png'), pygame.image.load('sprites/pygame_right_5.png'), pygame.image.load('sprites/pygame_right_6.png')]
 
@@ -44,10 +45,30 @@ def drawWindow():
         animCount += 1
     else:
         win.blit(playerStand, (x, y))
+
+    for bullet in bullets:
+        bullet.draw(win)
+
     pygame.display.update()
 
 
+class Bullet:
+    def __init__(self, x, y, radius, color, facing):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.facing = facing
+        self.vel = 8 * facing
+
+    def draw(self, win):
+        pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
+
+
+
+
 run = True
+bullets = []
 while run:
     clock.tick(30)
 
@@ -55,15 +76,32 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
+    for bullet in bullets:
+        if 500 > bullet.x > 0:
+            bullet.x += bullet.vel
+        else:
+            bullets.pop(bullets.index(bullet))
+
     keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_f]:
+        if last_move == 'right':
+            facing = 1
+        else:
+            facing = -1
+        if len(bullets) < 100:
+            bullets.append(Bullet(round(x + width // 2), round(y + height // 2), 5, (255, 0, 0), facing))
+
     if keys[pygame.K_LEFT] and x > 5:
         x -= speed
         left = True
         right = False
+        last_move = 'left'
     elif keys[pygame.K_RIGHT] and x < 500 - width - 5:
         x += speed
         left = False
         right = True
+        last_move = 'right'
     else:
         left = False
         right = False
